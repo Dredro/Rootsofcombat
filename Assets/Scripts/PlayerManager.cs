1efb2c7fdf210935833f4e686b32bfa7ec2bb05c
Dredro <110768094+Dredro@users.noreply.github.com>
2023-02-04 00:45:30 +0100

    c

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public List<Sprite> blueBody;
+    public List<Sprite> redBody;
+    public List<Sprite> greenBody;
+    public List<Sprite> yellowBody;
+
+    public AnimationClip blueWalk;
+    public AnimationClip redWalk;
+    public AnimationClip greenWalk;
+    public AnimationClip yellowWalk;
+}

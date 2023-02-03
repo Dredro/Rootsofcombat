6030a9139ba5c3cc42b94271bca8a60f3c551f86
Dredro <110768094+Dredro@users.noreply.github.com>
2023-02-03 23:27:38 +0100

    animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-  
+    private SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteBody;
     // Input Actions
     private PlayerInput playerInput;
     private InputAction moveAction;
@@ -21,6 +23,9 @@ public class PlayerController : MonoBehaviour
     private float jumpTimer;
     private Collider2D coll;
 
+    // Animation
+    private Animator animator;
+
     [Header("Ground Layer")]
     public LayerMask layerMask;
     [Header("Permable Layer")]
@@ -35,6 +40,8 @@ public class PlayerController : MonoBehaviour
     
     private void Start()
     {
+        spriteRenderer= GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
         rbody2D= GetComponent<Rigidbody2D>();
         coll= GetComponent<Collider2D>();
@@ -44,11 +51,11 @@ public class PlayerController : MonoBehaviour
     {
         OnUpdate();
         GroundCheck();
-        Jump();
         Descent();
     }
     private void FixedUpdate()
     {
+        Jump();
         Move();
     }
     private void OnUpdate()
@@ -65,7 +72,20 @@ public class PlayerController : MonoBehaviour
     }
    private void Move()
     {
-        rbody2D.velocity += new Vector2(move.x * moveSpeed, 0f);
+        animator.SetFloat("speed", Mathf.Abs(move.x));
+        rbody2D.velocity = new Vector2(move.x * moveSpeed, rbody2D.velocity.y);
+        if (move.x > 0)
+        {
+            spriteBody.flipX = true;
+            spriteRenderer.flipX= true;
+        }
+        if (move.x < 0)
+        {
+            spriteBody.flipX = false;
+            spriteRenderer.flipX = false;
+        }
+        
+        
     }
    private void GroundCheck()
     {
@@ -83,8 +103,9 @@ public class PlayerController : MonoBehaviour
     private void Descent()
     {
         bool permableUp,permableDown;
-        permableUp = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + transform.localScale.y / 2), 0.01f, permableLayerMask);
-        permableDown = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), 0.01f, permableLayerMask);
+        
+        permableUp = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + transform.localScale.y * 0.32f), 0.15f, permableLayerMask);
+        permableDown = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - transform.localScale.y * 0.32f), 0.2f, permableLayerMask);
         if ((permableDown && move.y < -0.9f)||permableUp)
         {
             coll.isTrigger= true;

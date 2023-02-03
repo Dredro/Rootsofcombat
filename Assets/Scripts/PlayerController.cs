d1278db8e87008b24dc6ce19177ab18ff9ba5611
Dredro <110768094+Dredro@users.noreply.github.com>
2023-02-03 19:27:46 +0100

    Move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,23 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    public LayerMask layerMask;
+
     private PlayerInput playerInput;
     private InputAction moveAction;
+    private InputAction fireAction;
+    private InputAction jumpAction;
+
+    
+    public bool isGround;
+
     private Rigidbody2D rbody2D;
     public float moveSpeed;
-    public Vector2 move;
+    public float jumpForce;
+    private Vector2 move;
+    private float jump;
+
+
     private void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -20,6 +32,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         OnUpdate();
+        GroundCheck();
+        Jump();
     }
     private void FixedUpdate()
     {
@@ -30,14 +44,28 @@ public class PlayerController : MonoBehaviour
         if(playerInput!= null)
         {
             moveAction = playerInput.actions["move"];
+            fireAction = playerInput.actions["fire"];
+            jumpAction = playerInput.actions["jump"];
         }
 
        move = moveAction.ReadValue<Vector2>();
-       
+      jump = jumpAction.ReadValue<float>();
     }
     void Move()
     {
-        rbody2D.velocity= move * moveSpeed;
+        rbody2D.velocity += new Vector2(move.x * moveSpeed, 0f);
     }
+    void GroundCheck()
+    {
+        isGround = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), 0.1f, layerMask);
+    }
+    void Jump()
+    {
+        if(isGround && jump == 1)
+        {
+           rbody2D.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+        }
+    }
+
 
 }

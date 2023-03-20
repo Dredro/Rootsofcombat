22d729429648f3582d36d8c60d03fa68c81b0805
ZenArcana <hubert.zochowski123@gmail.com>
2023-03-20 16:54:01 +0100

    SceneManager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField]private List<Scene> sceneSci;
+    [SerializeField] private List<Scene> sceneNow;
+    [SerializeField] private List<Scene> sceneRev;
+    [SerializeField] private List<Scene> sceneMed;
+    [SerializeField] private List<Scene> sceneNea;
+    [SerializeField] private Scene lobby;
+
+    public void LoadLobby()
+    {
+       
+    }
+            
+}

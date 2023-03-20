3df58f62b36a31e86138d99a17eec2884284fbbb
kendallwlubel <64282259+wlubel@users.noreply.github.com>
2023-03-20 16:58:09 +0100

    enumy part1

---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+    public enum Age{
+        SCI, NOW, REV, MID, NEA
+    }
+
+    public enum Colour{
+        RED, YELLOW, GREEN, BLUE
+    }
+
+    public enum Weapon_Type {
+        RANGED, MELEE
+    }
+
+}
